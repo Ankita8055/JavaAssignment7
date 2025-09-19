b8f82f9b0d73ba8989321c551969998ea12acfc7
Ankita8055 <155090107+Ankita8055@users.noreply.github.com>
2025-09-19 16:11:15 +0530

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,21 +1,14 @@
-package ass7problem5;
+package ass7problem4;
 
 public class Driver {
+
 	public static void main(String[] args) {
-		Parent p1 = new Parent();
-		System.out.println("method of parent class by object of parent class: ");
-		p1.dispParent();
+
+		Employee e1 = new Employee("Mike",23,1,"Mike",600000);
+		System.out.println(e1);
 		
-		System.out.println();
-		
-		Child c1 = new Child();
-		System.out.println("method of child class by object of child class: ");
-		c1.dispChild();
-		
-		System.out.println();
-		
-		System.out.println("method of parent class by object of child class: ");
-		c1.dispParent();
+		Person p1 = new Person("John",23);
+		System.out.println(p1);
 	}
 
 }
